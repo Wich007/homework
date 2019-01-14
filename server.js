a2b8e1f609288987c1487ff78c37ecc23b1b188a
Sirawich Wassanatip <siraw505@corp-ais900>
2019-01-14 10:22:40 +0700

    homework

---
+++ server.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const Hapi=require('hapi');
+
+// Create a server with a host and port
+const server=Hapi.server({
+    host:'localhost',
+    port:8000
+});
+
+// Add the route
+server.route({
+    method:'GET',
+    path:'/hello',
+    handler:function(request,h) {
+
+        return'hello world';
+    }
+});
+
+server.route({
+    method:'GET',
+    path:'/user/{name}/{age}/{email}',
+    handler:function(request,h) {
+
+        // return  "name : " + encodeURIComponent(request.params.name) + "<br>" +
+        //         "age : " + encodeURIComponent(request.params.age) + "<br>" +
+        //        "email : " + encodeURIComponent(request.params.email) ;
+
+        var object = {name: encodeURIComponent(request.params.name),
+            age : encodeURIComponent(request.params.age),
+            email: encodeURIComponent(request.params.email) 
+          };
+
+          return object;
+    }
+});
+
+
+let user = {
+    "name": "guutong",
+    "age": "99",
+    "email": "tong@odds.team"
+   };
+
+
+
+// Start the server
+const start =  async function() {
+
+    try {
+        await server.start();
+    }
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    console.log('Server running at:', server.info.uri);
+};
+
+start();
